82c3eb2d9ae7f4cbc5d9a4b937bab6766f7c5675
agungkurrrr <agungsimpati6@gmail.com>
2024-06-30 22:03:58 +0700

    add ceckin/outh add

---
+++ agung-main/agung-main/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:presensi_app/screen/attandance_recap_screen.dart';
+import 'package:presensi_app/screen/splash_screen.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+Future<void> main() async {
+  //untuk format tanggal indonesia
+  await initializeDateFormatting('id', null);
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'Presensi App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
+}
